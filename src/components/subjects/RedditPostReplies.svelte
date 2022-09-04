207f5aed77f5bf4f2568f8ad069a6f90a35c7a6d
caiges <caigesn@gmail.com>
2022-09-04 09:17:13 -0700

    Improve UX when replying

--- src/components/subjects/RedditPostReplies.svelte
+++ src/components/subjects/RedditPostReplies.svelte
@@ -1,10 +1,9 @@
 <script>
-	import moment from 'moment';
-	import RedditPostReplyForm from '@/components/subjects/RedditPostReplyForm.svelte';
-	import { JsonView } from '@zerodevx/svelte-json-view';
-	import ReplySheet from '../Source/ReplySheet.svelte';
+	import RedditReplies from '@/components/subjects/RedditReplies.svelte';
 
 	export let posts = [];
+
+	async function handleNewReply() {}
 </script>
 
 <div class="accordion open" id="post-accordian">
@@ -28,18 +27,8 @@
 				aria-labelledby={`post-header-${post.id}`}
 				data-bs-parent="#post-accordian"
 			>
-				<div class="replies">
-					{#each post.replies as reply}
-						<div class="reply">
-							<div class="body">{reply.data.body}</div>
-							<div class="created">
-								{moment(reply.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a')}
-							</div>
-						</div>
-					{/each}
-				</div>
 				<div class="accordion-body">
-					<RedditPostReplyForm {post} />
+					<RedditReplies {post} />
 				</div>
 			</div>
 		</div>
--- src/components/subjects/RedditPostReplyForm.svelte
+++ src/components/subjects/RedditPostReplyForm.svelte
@@ -1,35 +1,51 @@
 <script>
+	import { createReply } from '$lib/db';
+	import { createEventDispatcher } from 'svelte';
+
 	export let post;
 	let replyValue = '';
+	let waiting = false;
+	const dispatch = createEventDispatcher();
 
 	async function handleReply() {
-		const path = `${import.meta.env.VITE_BASE_URL}/api/subjects/${post.subject_id}/posts/${
-			post.id
-		}/replies/reddit`;
+		try {
+			waiting = true;
+			const path = `${import.meta.env.VITE_BASE_URL}/api/subjects/${post.subject_id}/posts/${
+				post.id
+			}/replies/reddit`;
 
-		const createReplyResponse = await fetch(path, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				reply: replyValue
-			})
-		});
-		console.log('reply', createReplyResponse);
+			const createReplyResponse = await fetch(path, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					reply: replyValue
+				})
+			});
+			const newReply = await createReplyResponse.json();
+			dispatch('replyCreated', newReply);
+		} catch (e) {
+			console.error('could not create reply', e);
+		}
+
+		waiting = false;
 	}
 </script>
 
 <form>
 	<div class="mb-3">
-		<label for="reply" class="form-label">Reply</label>
+		<label for="reply" class="form-label">New Reply</label>
 		<textarea
 			class="form-control"
 			id={`reply-post-${post.id}`}
 			aria-describedby="replyHelp"
 			bind:value={replyValue}
+			disabled={waiting}
 		/>
 		<div id="replyHelp" class="form-text">Reply to Reddit submission.</div>
 	</div>
-	<button type="button" class="btn btn-primary" on:click={handleReply}>Reply</button>
+	<button type="button" class="btn btn-primary" on:click={handleReply} disabled={waiting}
+		>Reply</button
+	>
 </form>
