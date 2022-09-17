293118f8bc6bfb442644bf40346ce81c55ee3034
caiges <caigesn@gmail.com>
2022-09-16 20:58:35 -0700

    Wire up twitter login

--- src/routes/api/auth/twitter/callback/+server.js
+++ src/routes/api/auth/twitter/callback/+server.js
@@ -10,7 +10,14 @@ export async function GET({ request, url }) {
   const oauth_token = url.searchParams.get('oauth_token');
   const oauth_verifier = url.searchParams.get('oauth_verifier');
   const cookies = parse(request.headers.get('cookie') || '');
-  const { oauth_token_secret } = cookies['twitter-auth'];
+  const { oauth_token_secret } = JSON.parse(cookies['twitter-auth']);
+
+  const twitterConfig = {
+    appKey: import.meta.env.VITE_TWITTER_API_KEY,
+    appSecret: import.meta.env.VITE_TWITTER_API_SECRET,
+    accessToken: oauth_token,
+    accessSecret: oauth_token_secret,
+  };
 
   const twitterClient = new TwitterApi({
     appKey: import.meta.env.VITE_TWITTER_API_KEY,
@@ -19,48 +26,46 @@ export async function GET({ request, url }) {
     accessSecret: oauth_token_secret,
   });
 
-  const { client: loggedClient, accessToken, accessSecret } = await twitterClient.login(oauth_verifier);
-  console.log(await loggedClient.currentUser())
+  try {
+    const { client: loggedClient, accessToken, accessSecret } = await twitterClient.login(oauth_verifier);
+    const twitterUser = await loggedClient.currentUser();
 
-  if (false) {
-    try {
-      // Create or get user.
-      const platformWhere = {
-        reddit_user_id: profile.id
-      };
-      const platformFields = {
-        name: profile.name,
-        avatar_url: profile.snoovatar_img,
-        reddit_user_id: profile.id,
-        reddit_username: profile.name
-      }
-
-      // Create database client.
-      const prisma = new PrismaClient()
-
-      // Get or create user.
-      const user = await prisma.users.upsert({
-        create: platformFields,
-        update: platformFields,
-        where: platformWhere
-      });
-
-      const session = {
-        user,
-        redditTokens: {
-          accessToken, refreshToken
-        }
-      };
-
-      const status = 302;
-      const headers = {
-        'Location': state.redirectURL,
-        'set-cookie': [serialize('session', JSON.stringify(session), { path: '/' })],
-      }
-
-      return new Response(null, { status, headers });
-    } catch (e) {
-      throw error(500, `could not authenticate with Twitter: ${e}`);
+    // Create or get user.
+    const platformWhere = {
+      twitter_user_id: twitterUser.id
+    };
+    const platformFields = {
+      name: twitterUser.screen_name,
+      avatar_url: twitterUser.profile_image_url_https,
+      twitter_user_id: twitterUser.id,
+      twitter_username: twitterUser.screen_name
     }
+
+    // Create database client.
+    const prisma = new PrismaClient()
+
+    // Get or create user.
+    const user = await prisma.users.upsert({
+      create: platformFields,
+      update: platformFields,
+      where: platformWhere
+    });
+
+    const session = {
+      user,
+      twitterTokens: {
+        accessToken, accessSecret
+      }
+    };
+
+    const status = 302;
+    const headers = {
+      'Location': '/',
+      'set-cookie': [serialize('session', JSON.stringify(session), { path: '/' })],
+    }
+
+    return new Response(null, { status, headers });
+  } catch (e) {
+    throw error(500, `could not authenticate with Twitter: ${e}`);
   }
 }
