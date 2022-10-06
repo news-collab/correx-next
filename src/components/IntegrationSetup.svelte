aa27ca8f864c020c5359e1fb0a18e1c156b44641
caiges <caigesn@gmail.com>
2022-10-05 21:59:41 -0700

    Check for user

--- src/components/IntegrationSetup.svelte
+++ src/components/IntegrationSetup.svelte
@@ -1,9 +1,9 @@
 <script>
   import { getStores, navigating, page } from '$app/stores';
-  export let user = {};
+  export let user;
 </script>
 
-{#if $page.url.pathname != '/integrations' && (!user.reddit_access_token || !user.twitter_access_token)}
+{#if $page.url.pathname != '/integrations' && user && (!user.reddit_access_token || !user.twitter_access_token)}
   <div class="alert alert-warning" role="alert">
     You don't have all integrations configured and won't be able interact with the missing
     integration's platform specific content without <a href="/integrations">connecting</a> your account
