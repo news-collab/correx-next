8b2fb2247684f00df2718d3ebe3b243f80075c88
caiges <caigesn@gmail.com>
2022-10-26 17:41:38 -0700

    Update reply interface to show author replies

--- src/components/replies/TwitterReply.svelte
+++ src/components/replies/TwitterReply.svelte
@@ -25,40 +25,62 @@
   })();
 </script>
 
-{#if loading && replyComments.length === 0}
-  <div class="loading">
-    <Circle2 size="60" color="#FF3E00" unit="px" duration="1s" />
+<div class="comment-replies">
+  <div class="comment">
+    <div class="header">
+      <p>
+        <span class="author"
+          ><a
+            href={`https://www.twitter.com/${reply.author.twitter_username}`}
+            target="twitter_${reply.author.twitter_user_id}">{reply.author.twitter_username}</a
+          ></span
+        >
+        wrote on
+        <span class="created">
+          {moment(reply.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a')}
+        </span>
+      </p>
+    </div>
+    <div class="body"><p>{@html reply.data.body}</p></div>
+    <div class="footer">
+      <a href={reply.data.permalink} target="twitter_${reply.platform_id}">permalink</a>
+    </div>
   </div>
-{/if}
-{#if replyComments.length > 0}
-  <div class="comment-replies">
-    {#each replyComments as comment}
-      <div class="comment">
-        <div class="header">
-          <p>
-            <span class="author"
-              ><a
-                href={`https://www.twitter.com/${comment.author.username}`}
-                target="twitter_${comment.tweet.author_id}">{comment.author.username}</a
-              ></span
+  {#if loading && replyComments.length === 0}
+    <div class="loading">
+      <Circle2 size="60" color="#FF3E00" unit="px" duration="1s" />
+    </div>
+  {/if}
+  {#if replyComments.length > 0}
+    <div class="replies">
+      {#each replyComments as comment}
+        <div class="comment">
+          <div class="header">
+            <p>
+              <span class="author"
+                ><a
+                  href={`https://www.twitter.com/${comment.author.username}`}
+                  target="twitter_${comment.tweet.author_id}">{comment.author.username}</a
+                ></span
+              >
+              wrote on
+              <span class="created">
+                {moment(comment.tweet.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a')}
+              </span>
+            </p>
+          </div>
+          <div class="body"><p>{@html comment.tweet.text}</p></div>
+          <div class="footer">
+            <a
+              href={`https://www.twitter.com/${comment.author.username}/status/${comment.tweet.id}`}
+              target="twitter_${comment.tweet.id}">permalink</a
             >
-            wrote on
-            <span class="created">
-              {moment(comment.tweet.created_at).format('dddd, MMMM Do YYYY, h:mm:ss a')}
-            </span>
-          </p>
+          </div>
         </div>
-        <div class="body"><p>{@html comment.tweet.text}</p></div>
-        <div class="footer">
-          <a
-            href={`https://www.twitter.com/${comment.author.username}/status/${comment.tweet.id}`}
-            target="twitter_${comment.tweet.id}">permalink</a
-          >
-        </div>
-      </div>
-    {/each}
-  </div>
-{/if}
+      {/each}
+    </div>
+  {/if}
+</div>
 
 <style>
   .loading {
@@ -78,6 +100,12 @@
 
   .footer a {
     color: #767676;
+  }
+
+  .replies {
+    margin-left: 20px;
+    padding-left: 10px;
+    border-left: 1px solid #dddddd;
   }
 
   .replies {
